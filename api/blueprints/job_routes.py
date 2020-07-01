2c43e5dc9308d118b9607d0feaded8a60baf4010
Corey Lanier <iam@coreylanier.com>
2020-06-30 21:03:49 -0400

    created model for jobs

---
+++ api/blueprints/job_routes.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, jsonify, request
+import mongoengine as me
+import bcrypt
+import json
+import jwt
+from dotenv import load_dotenv
+import os
+
+# loading app secret
+load_dotenv()
+secret = os.getenv('SECRET')
+
+# defining blueprint
+job_routes = Blueprint('job_routes', __name__)
+
+# create user model
+class Job(me.Document):
+	title = me.StringField(required=True)
+	company = me.StringField(required=True)
+	date_posted = me.DateTimeField(required=False)
+	date_applied = me.DateTimeField(required=True)
+	posting_url = me.StringField(required=False)
+	direct_posting = me.BooleanField(required=True, default=False)
+	contact_name = me.StringField(required=False)
+	contact_email = me.StringField(required=True)
+	active = me.BooleanField(required=True, default=True)
+	feedback = me.StringField(required=False)
+	ghosted = me.BooleanField(required=False)
+
+
