78d5c406e2601fb6d7c33c7a2986d407b39204e9
Corey Lanier <iam@coreylanier.com>
2020-07-10 20:09:24 -0400

    added logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./App.css";
 import UserContextProvider from "./contexts/UserContext";
 import Signup from "./components/Signup";
+import logo from "./assets/logo.png";
 
 function App() {
 	return (
 		<div className="App">
+			<div className="logo">
+				<img src={logo} alt="Job Hunt Logo" />
+			</div>
 			<UserContextProvider>
 				<div className="container">
 					<Signup />
