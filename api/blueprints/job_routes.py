dcb36c9705ab2b33e3f9fb1afcd8d26b6d7db970
Corey Lanier <iam@coreylanier.com>
2020-07-12 21:54:53 -0400

    added endpoint for updating jobs

--- api/blueprints/job_routes.py
+++ api/blueprints/job_routes.py
@@ -15,38 +15,65 @@ secret = os.getenv('SECRET')
 job_routes = Blueprint('job_routes', __name__)
 
 # create user model
+
+
 class Job(me.Document):
-	title = me.StringField(required=True)
-	company = me.StringField(required=True)
-	date_posted = me.DateTimeField(required=False)
-	date_applied = me.DateTimeField(required=True)
-	posting_url = me.StringField(required=False)
-	direct_posting = me.BooleanField(required=True, default=False)
-	contact_name = me.StringField(required=False)
-	contact_email = me.StringField(required=True)
-	active = me.BooleanField(required=True, default=True)
-	feedback = me.StringField(required=False)
-	ghosted = me.BooleanField(required=False)
-	user = me.ReferenceField(User, required=True)
+    title = me.StringField(required=True)
+    company = me.StringField(required=True)
+    date_posted = me.DateTimeField(required=False)
+    date_applied = me.DateTimeField(required=True)
+    posting_url = me.StringField(required=False)
+    direct_posting = me.BooleanField(required=True, default=False)
+    contact_name = me.StringField(required=False)
+    contact_email = me.StringField(required=True)
+    active = me.BooleanField(required=True, default=True)
+    feedback = me.StringField(required=False)
+    ghosted = me.BooleanField(required=False)
+    user = me.ReferenceField(User, required=True)
+    scam = me.BooleanField(required=False)
 
 
 # create a job
 @job_routes.route('', methods=['POST'])
 def create_job():
-	post_data = request.get_json()
-	new_job = Job(
-		title=post_data.get('title'),
-		company=post_data.get('company'),
-		date_posted=post_data.get('date_posted'),
-		date_applied=post_data.get('date_applied'),
-		posting_url=post_data.get('posting_url'),
-		direct_posting=post_data.get('direct_posting'),
-		contact_name=post_data.get('contact_name'),
-		contact_email=post_data.get('contact_email'),
-		user=post_data.get('user')
-	)
-	new_job.save()
-	return jsonify({
-		'status': 'success',
-		'data': new_job
-	})
+    post_data = request.get_json()
+    new_job = Job(
+        title=post_data.get('title'),
+        company=post_data.get('company'),
+        date_posted=post_data.get('date_posted'),
+        date_applied=post_data.get('date_applied'),
+        posting_url=post_data.get('posting_url'),
+        direct_posting=post_data.get('direct_posting'),
+        contact_name=post_data.get('contact_name'),
+        contact_email=post_data.get('contact_email'),
+        user=post_data.get('user')
+    )
+    new_job.save()
+    return jsonify({
+        'status': 'success',
+        'data': new_job
+    })
+
+# edit a job
+
+
+@job_routes.route('/<id>', methods=['PUT'])
+def update_job(id):
+    # get request data
+    post_data = request.get_json()
+    # get job from db
+    job = Job.objects.get(pk=id)
+    job.update(**{
+        "set__title": post_data.get('title'),
+        "set__company": post_data.get('company'),
+        "set__contact_name": post_data.get('contact_name'),
+        "set__contact_email": post_data.get('contact_email'),
+        "set__active": post_data.get('active'),
+        "set__ghosted": post_data.get('ghosted'),
+        "set__feedback": post_data.get('feedback'),
+        "set__scam": post_data.get('scam')
+    })
+    return jsonify({
+        'status': 'success',
+        'data': job
+    })
