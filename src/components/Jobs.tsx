a8c6edbec933d4180f7514a39deed3ef9aab6eac
Corey Lanier <iam@coreylanier.com>
2020-07-15 21:42:47 -0400

    created conditionals for application status

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -12,13 +12,28 @@ const Jobs = (props: IJobProps) => {
 	const dateApplied: string = dt2.toLocaleString("en-us");
 	const datePostedArr: Array<string> = datePosted.split(",");
 	const dateAppliedArr: Array<string> = dateApplied.split(",");
+	const { active, positive, scam, ghosted } = props.job;
 	return (
-		<div className="job-grid-row">
+		<div
+			className={`job-grid-row ${
+				active && positive
+					? "positive"
+					: active && !positive
+					? "active"
+					: scam
+					? "scam"
+					: ghosted
+					? "ghosted"
+					: !active && positive
+					? "rejected-later"
+					: "rejected"
+			}`}
+		>
 			<div>{props.job.title}</div>
 			<div>{props.job.company}</div>
 			<div>{datePostedArr[0]}</div>
 			<div>{dateAppliedArr[0]}</div>
-			<div></div>
+			<div>{active ? "Active" : "Rejected"}</div>
 			<div>
 				<Link to={`/jobs/${props.job._id.$oid}`}>{">>"}</Link>
 			</div>
